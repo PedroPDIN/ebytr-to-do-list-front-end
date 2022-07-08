6129bc422d7038f31c3c844e743bdc69f6d99469
Pedro Lima <pedro.oficial1969@gmail.com>
2022-07-08 18:26:17 -0300

    style: adiciona estilização as lista de tarefas.

--- src/pages/ListTask/styled.js
+++ src/pages/ListTask/styled.js
@@ -9,13 +9,15 @@ const SListTask = styled.div`
   section {
     background-color: white;
     border-radius: 20px;
+    box-shadow: 0 0 1em black;
     margin-top: 30px;
-    max-width: 500px;
+    width: 50em;
   }
 
   p {
     margin-left: 10px;
     font-size: 20px;
+    text-align: center;
   }
 
   .task {
@@ -23,28 +25,66 @@ const SListTask = styled.div`
   }
 
   .task p {
-    max-width: 400px !important;
+    max-width: 95%;
+    font-size: 24px;
   }
 
   .task-info {
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 99%;
   }
 
   .task-info p:nth-child(1) {
     background-color: #E9F0EB;
     border: 1px solid black;
     border-radius: 10px;
-    margin-left: 10px;
     padding: 5px;
     text-align: center;
+    width: 200px;
   }
 
   .task-info p:nth-child(2) {
+    background-color: black;
     border: 1px solid black;
     border-radius: 10px;
-    margin-left: 10px;
+    color: white;
+    margin-top: -10px;
     padding: 5px;
     text-align: center;
+    width: 200px;
+  }
+
+  // ======================
+  .task-buttons {
+    align-items: flex-end;
+    display: flex;
+    justify-content: center;
+  }
+  
+  .task-buttons button {
+    margin: 0px 5px 20px 5px;
+    border-radius: 8px;
+    width: 50px;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .task-buttons button:hover {
+    transform: translateY(6px);
+  }
+
+  .task-buttons button:nth-child(1):hover {
+    color: #FA9E26;
+  }
+
+  .task-buttons button:nth-child(2):hover {
+    background-color: #F0260E;
+    color: black;
+  }
+
+  .task-icon {
+    font-size: 30px;
   }
 }
 
@@ -72,21 +112,24 @@ const SListTask = styled.div`
   }
   
   .task p {
-    max-width: 95%;;
+    max-width: 95%;
+    font-size: 24px;
   }
 
   .task-info {
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    width: 99%;
   }
 
   .task-info p:nth-child(1) {
     background-color: #E9F0EB;
     border: 1px solid black;
     border-radius: 10px;
-    margin-left: 10px;
     padding: 5px;
     text-align: center;
+    width: 200px;
   }
 
   .task-info p:nth-child(2) {
@@ -94,9 +137,10 @@ const SListTask = styled.div`
     border: 1px solid black;
     border-radius: 10px;
     color: white;
-    margin-left: 10px;
+    margin-top: -10px;
     padding: 5px;
     text-align: center;
+    width: 200px;
   }
   
   .task-buttons {
